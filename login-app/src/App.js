1b3ce145c00076d3f466a20b36e05df26c49fe59
Riya <riyathakurprof@gmail.com>
2023-09-30 20:35:18 +0530

    new changes added in login app

--- login-app/src/App.js
+++ login-app/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import Form from './form';
+import Form from './Form';
 function App() {
   return (
   
--- login-app/src/form.js
+++ login-app/src/form.js
@@ -1,42 +1,64 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-const form = () => {
+const Form = () => {
+
+ const data={ name:'',  email:'', password:'', phone:''};
+ const [inputData, setInputData] = useState(data)
+ const [flag, setFlag]=useState(false)
+ useEffect(()=>{
+  console.log("Registered")}, [flag])
+
+ function handleData(e){
+  setInputData({...inputData, [e.target.name]:e.target.value})
+ console.log(inputData)
+ }
+
+  function hanleSubmit(e)
+  {
+    e.preventDefault();
+    if(!inputData.name || !inputData.email || !inputData.password || !inputData.phone)
+    {
+      alert("All fields are Mendatory")
+    }
+    else
+    {
+      setFlag(true)
+    }
+  }
+  
+
   return (
-    <div>
-        <form className='container'>
-            <div className='header'>
+    <>
+    <pre>{(flag)?<h2 className='ui-define'> Hello {inputData.name}, You've successfully Registered</h2>:"" }</pre>
+        <div>
+        <form className='container' onSubmit={hanleSubmit}>
+            <div className='header'> 
                 <h1> Registration form</h1>
                 <div>
                 <label for="fname">Name:</label><br></br>
-                <input type ="text" placeholder='enter your name' name='name'></input><br></br>
+                <input type ="text" placeholder='enter your name' name='name' value={inputData.name} onChange={handleData}></input>
                 </div>
                 <div> 
-                <label for="fname">Email:</label><br></br>
-                <input type ="text" placeholder='enter your email' name='email'></input>
+                <label for="email">Email:</label><br></br>
+                <input type ="text" placeholder='enter your email' name='email' value={inputData.email} onChange={handleData}></input>
                 </div>
                 <div>
-                <label for="fname">password:</label><br></br>
-                <input type ="text" placeholder='enter your password' name='password'></input>
+                <label for="password">password:</label><br></br>
+                <input type ="password" placeholder='enter your password' name='password' value={inputData.password} onChange={handleData}></input>
                 </div>
                 <div>
-                <label for="fname">Phone:</label><br></br>
-                <input type ="text" placeholder='enter your phone' name='phone'></input>
+                <label for="phone">Phone:</label><br></br>
+                <input type ="text" placeholder='enter your phone' name='phone' value={inputData.phone} onChange={handleData}></input>
                 </div>
                 <div>
                     <button type='submit'>Submit</button>
                 </div>
-
-
-
-
             </div>
-
-
-        </form>
-
-      
+        </form>  
     </div>
+    </>
+
   );
 }
 
-export default form;
+export default Form;
